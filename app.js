131bd2a23fff93df1ed399ffa2fcf9e2e6bae3e7
Bryan Kenny <b.93.kenny@gmail.com>
2019-01-15 00:57:59 +0000

    the snakehead positiion changes on key press

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ var blockSize = 20
 var headX = 0
 var headY = 0
 
+// snakehead speed
+var speedX = 0
+var speedY = 0
+
 function setup() {
   createCanvas(400, 400);
   frameRate(10)
@@ -17,10 +21,32 @@ function setup() {
 }
 
 function draw() {
-  background(0, 0, 0);
+  background(0, 0, 0)
+  // draw head
   fill(255)
   rect(headX * blockSize,
        headY * blockSize,
       blockSize,
       blockSize)
+
+  // update position of snakes head when key is pressed
+  headX = headX + speedX
+  headY = headY + speedY
+}
+
+function keyPressed() {
+
+  if(key == 'w') {
+    speedX = 0
+    speedY = -1
+  } else if(key == 's') {
+      speedX = 0
+      speedY = 1
+  } else if(key == 'a') {
+      speedX = -1
+      speedY = 0
+  } else if(key == 'd') {
+      speedX = 1
+      speedY = 0
+  }
 }
